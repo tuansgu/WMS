34034a4c85e683f215c89486df7da21860307958
tuansgu <nguyentuan01082003t@gmail.com>
2024-07-24 23:45:25 +0700

    None

--- client/src/components/order.tsx
+++ client/src/components/order.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 interface Item {
   id: number;
@@ -14,6 +18,7 @@ const Order: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [searchResults, setSearchResults] = useState<Item[]>([]);
   const [selectedItems, setSelectedItems] = useState<Item[]>([]);
+  const [currentStep, setCurrentStep] = useState<number>(1);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const term = e.target.value;
@@ -31,21 +36,33 @@ const Order: React.FC = () => {
     setSearchResults([]);  // Hide search results
   };
 
+  const handleSave = () => {
+    setCurrentStep(prevStep => prevStep + 1);
+    
+  };
+
   return (
     <>
-      <div style={{ backgroundColor: '#E7E9EB' }}>
+      <div style={{ backgroundColor: '#E7E9EB', height: '100vh', overflow: 'auto' }} >
         <div style={{ marginTop: '80px', margin: '40px' }}>
           <div className="accordion" id="accordionExample" style={{ padding: '40px' }}>
-            <div className="accordion-item" style={{ marginBottom: '40px' }}>
-              <div className="accordion-header mt-3 me-3 ms-3 mb-3" id="headingOne" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                <button className="btn fs-2 fw-bold ms-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <div className="accordion-item accordion-transition: .15s" style={{ marginBottom: '40px' }}>
+              <div className="accordion-header mt-3 me-3 ms-3 mb-3" id="headingOne">
+                <button
+                  className="btn fs-2 fw-bold ms-5"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#1"
+                  aria-expanded={currentStep === 1}
+                  aria-controls="1"
+                >
                   Which EveryShip warehouse are you shipping to?
                 </button>
-                <div id="collapseOne" className="accordion-collapse collapse show" style={{ marginLeft: '70px' }} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="1" className={`accordion-collapse collapse ${currentStep === 1 ? 'show' : ''}`} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                   Step 1 of
                 </div>
               </div>
-              <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div id="1" className={`accordion-collapse collapse ${currentStep === 1 ? 'show' : ''}`} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div className="accordion-body" style={{ marginTop: '50px' }}>
                   <div className="form-control-lg" style={{ backgroundColor: "#E7E9EB", marginLeft: '54px' }}>
                     Head up!
@@ -65,22 +82,35 @@ const Order: React.FC = () => {
                   </div>
                 </div>
                 <div className="border">
-                  <button className="btn me-3 mt-3 mb-3 fs-5" style={{ backgroundColor: "#2596be", color: "white", marginLeft: '70px' }}>Save & Continue</button>
+                  <button
+                    className="btn me-3 mt-3 mb-3 fs-5"
+                    style={{ backgroundColor: "#2596be", color: "white", marginLeft: '70px' }}
+                    onClick={handleSave}
+                  >
+                    Save & Continue
+                  </button>
                   <button className="btn fs-5" style={{ color: '#2596be' }}>Cancel</button>
                 </div>
               </div>
             </div>
-            <div className="accordion-item">
-              <div className="accordion-header mt-3 me-3 ms-3 mb-3" id="headingTwo" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                <button className="btn fs-2 fw-bold ms-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <div className="accordion-item" style={{ marginBottom: '40px' }}>
+              <div className="accordion-header mt-3 me-3 ms-3 mb-3" id="headingTwo">
+                <button
+                  className="btn fs-2 fw-bold ms-5"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#2"
+                  aria-expanded={currentStep === 2}
+                  aria-controls="2"
+                >
                   Search for item(s) you're sending to EveryShip
                 </button>
-                <div id="collapseTwo" className="accordion-collapse collapse show" style={{ marginLeft: '70px' }} aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div id="2" className={`accordion-collapse collapse ${currentStep === 2 ? 'show' : ''}`} aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                   Step 2 of
                 </div>
               </div>
-              <div id="collapseTwo" className="accordion-collapse collapse show mt-3" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div className="accordion-body" style={{ marginTop: '50px' }}>
+              <div id="2" className={`accordion-collapse collapse ${currentStep === 2 ? 'show' : ''}`} aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div className="accordion-body" style={{ marginTop: '50px'}}>
                   <div style={{ marginLeft: '54px', position: 'relative' }}>
                     <input
                       type="text"
@@ -158,7 +188,66 @@ const Order: React.FC = () => {
                   </div>
                 </div>
                 <div className="border">
-                  <button className="btn me-3 mt-3 mb-3 fs-5" style={{ backgroundColor: "#2596be", color: "white", marginLeft: '70px' }}>Save & Continue</button>
+                  <button
+                    className="btn me-3 mt-3 mb-3 fs-5"
+                    style={{ backgroundColor: "#2596be", color: "white", marginLeft: '70px' }}
+                    onClick={handleSave}
+                  >
+                    Save & Continue
+                  </button>
+                  <button className="btn fs-5" style={{ color: '#2596be' }}>Cancel</button>
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item" style={{ marginBottom: '40px' }} >
+              <div className="accordion-header mt-3 me-3 ms-3 mb-3" id="headingThree">
+                <button
+                  className="btn fs-2 fw-bold ms-5"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#3"
+                  aria-expanded={currentStep === 3}
+                  aria-controls="3"
+                >
+                  How will your inventory arrive to EveryShip's fulfillment center?
+                </button>
+                <div id="3" className={`accordion-collapse collapse ${currentStep === 3 ? 'show' : ''}`} aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                  Step 3 of
+                </div>
+              </div>
+              <div id="3" className={`accordion-collapse collapse ${currentStep === 3 ? 'show' : ''}`} aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div className="accordion-body" style={{ marginTop: '50px' }}>
+                  <div>
+                    <span style={{ marginLeft: '54px' }}>
+                      Shipping Type:
+                      <span data-tooltip-id="infoTooltip" style={{ cursor: 'pointer' }}>
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                      </span>
+                      <Tooltip id="infoTooltip" place="right" content="This text explains the shipping type..." />
+                    </span>
+                    <div className="form-check " style={{ marginLeft: '54px' }}>
+                      <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1" />
+                      <label className="form-check-label">Parcel(standard shipping)</label>
+                    </div>
+                    <div className="form-check " style={{ marginLeft: '54px' }}>
+                      <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2" />
+                      <label className="form-check-label">Palletized container or LTL(less than-truckload)</label>
+                    </div>
+                    <div className="form-check" style={{ marginLeft: '54px' }}>
+                      <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="3" />
+                      <label className="form-check-label">Floor loaded container</label>
+                    </div>
+                    <span style={{ color: '#adb5bd', marginLeft: '80px' }}>We require 1 WRO per container.</span>
+                  </div>
+                </div>
+                <div className="border">
+                  <button
+                    className="btn me-3 mt-3 mb-3 fs-5"
+                    style={{ backgroundColor: "#2596be", color: "white", marginLeft: '70px' }}
+                    onClick={handleSave}
+                  >
+                    Save & Continue
+                  </button>
                   <button className="btn fs-5" style={{ color: '#2596be' }}>Cancel</button>
                 </div>
               </div>
